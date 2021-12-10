802061646c61bd6555c8e6f6aa35b90be1ce8ce7
ddthien-coder <jungjihoonkr97@gmail.com>
2021-12-10 22:56:18 +0700

    update

--- src/main/java/com/devteam/BApiApplication.java
+++ src/main/java/com/devteam/BApiApplication.java
@@ -2,10 +2,19 @@ package com.devteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Controller
 @SpringBootApplication
 public class BApiApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "B-DIGITAL API SERVER START";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BApiApplication.class, args);
